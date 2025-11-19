9150dd2db10680b627cf5ae7d628680981029a63
fidhaparvin95-prog <fidhaparvin95@gmail.com>
2025-11-19 18:48:03 +0000

    Update editprofile.php

--- Zudo/editprofile.php
+++ Zudo/editprofile.php
@@ -4,9 +4,9 @@ $name=$_POST["adminname"];
 $un=$_POST["username"];
 $pwd=$_POST["pwd"];
 
-$conn=mysqli_connect("vesta.uclan.ac.uk","FPRifas-ali","NLX7ytZS");
+$conn=mysqli_connect("Localhost","root","");
 
-mysqli_select_db($conn,'FPRifas-ali');
+mysqli_select_db($conn,'zudo');
 $q="UPDATE `adminlogin` SET `name`='$name',`username`='$un',`password`='$pwd' WHERE userno=".$_SESSION["suserno"];
 mysqli_query($conn,$q);
 mysqli_close($conn);
