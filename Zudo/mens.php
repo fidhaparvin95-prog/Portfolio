710e5c80fc59c773f7fcf2e4a92e66f8ca91023f
fidhaparvin95-prog <fidhaparvin95@gmail.com>
2025-11-19 18:55:53 +0000

    Update mens.php

--- Zudo/mens.php
+++ Zudo/mens.php
@@ -30,8 +30,8 @@
     
         
         <?php
-    $conn = mysqli_connect("vesta.uclan.ac.uk", "FPRifas-ali", "NLX7ytZS");
-    mysqli_select_db($conn, 'FPRifas-ali');
+    $conn = mysqli_connect("Localhost", "root", "");
+    mysqli_select_db($conn, 'zudo');
     $q = "SELECT * FROM product WHERE category = 'mens' LIMIT 3;";
     $rs = mysqli_query($conn, $q); ?>
     <div class="container">
