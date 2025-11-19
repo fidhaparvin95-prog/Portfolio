99314ac7c3dac009a867fd44f96ff2bd90673a53
fidhaparvin95-prog <fidhaparvin95@gmail.com>
2025-11-19 18:40:48 +0000

    Update deleteadmincontrol.php

--- Zudo/deleteadmincontrol.php
+++ Zudo/deleteadmincontrol.php
@@ -2,8 +2,8 @@
 session_start();
 
 $id=$_POST["usernmbr"];
-$conn=mysqli_connect("vesta.uclan.ac.uk","FPRifas-ali","NLX7ytZS");
-mysqli_select_db($conn,'FPRifas-ali');
+$conn=mysqli_connect("Localhost","root","");
+mysqli_select_db($conn,'zudo');
 $q="SELECT * FROM `adminlogin` WHERE userno='$id'";
 $rs=mysqli_query($conn,$q);
 $row=mysqli_fetch_array($rs);
